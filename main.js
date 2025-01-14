84eb41db3176e6f860ab7cf70395277b9cf9ab88
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-01-14 20:14:49 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper/bundle';
+import { Fancybox } from "@fancyapps/ui";
+import { Mask, MaskInput } from "maska"
+
+import './sass/_app.scss';
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import 'swiper/css/bundle';
+
+new MaskInput("[data-maska]") // for masked input
+
+Fancybox.bind("[data-fancybox]", {});
