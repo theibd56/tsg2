c765db5b67300a340e7c2d1a1eafdee3dd26f93e
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-01-14 21:37:14 +0700

    hit hit section has been created

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ import 'swiper/css/bundle';
 new MaskInput("[data-maska]") // for masked input
 
 Fancybox.bind("[data-fancybox]", {});
+
+const hitSlider = new Swiper('.hit-slider .swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 600,
+    loop: false,
+    pagination: {
+        el: '.hit-slider__pagination',
+        clickable: true,
+    },
+})
