e2ca60516e76365fbb754b323de5735ede1ca25e
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-01-16 22:38:17 +0700

    map section has been created

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ new MaskInput("[data-maska]") // for masked input
 
 Fancybox.bind("[data-fancybox]", {});
 
+
+const script = document.createElement('script');
+script.src = "https://api-maps.yandex.ru/2.1/?lang=ru_RU";
+script.type = "text/javascript";
+document.head.appendChild(script);
+
+script.onload = () => {
+    ymaps.ready(init);
+
+    function init() {
+        const map = new ymaps.Map('map', {
+            center: [55.030204, 82.920430],
+            zoom: 10,
+            controls: []
+        });
+    }
+};
+
+
 const hitSlider = new Swiper('.hit-slider .swiper', {
     slidesPerView: 3,
     spaceBetween: 30,
